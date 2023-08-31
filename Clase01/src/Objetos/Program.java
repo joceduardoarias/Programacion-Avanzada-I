31c8fe9b6dbc912ffbe51f3e0c0cd495eaec9e30
Jose Arias <josee.ariasmelo@gmail.com>
2023-08-31 17:56:51 -0300

    Add first project develop in Java

---
+++ Clase01/src/Objetos/Program.java
@@ -0,0 +1,11 @@
+package Objetos;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "hello word";
+		System.err.println(str);
+	}
+
+}
