0a6899aaa724c3c239f4c82dcba7e2bbd79e70ce
Jose Arias <josee.ariasmelo@gmail.com>
2023-09-05 11:37:22 -0300

    feature/ Instantiating objects in java.

--- Clase01/src/Objetos/Program.java
+++ Clase01/src/Objetos/Program.java
@@ -6,6 +6,13 @@ public class Program {
 		// TODO Auto-generated method stub
 		String str = "hello word";
 		System.err.println(str);
+
+		// Instantiating person object
+		Persona personaUno = new Persona();
+		personaUno.dni = 12345678;
+		personaUno.nombre = "Juan";
+		
+		System.out.println("Usuario: " + personaUno.dni + "\n" + "DNI: " + personaUno.nombre);
 	}
 
 }
